b92e4c5adccc1dab587abdf7f116f40f5ec8f979
Jason Woodruff <jwoodruff25@basecampcodingacademy.org>
2025-02-12 13:05:14 -0600

    second java added

--- src/Main.java
+++ src/Main.java
@@ -8,92 +8,89 @@ public class Main {
 
       System.out.println("Hello Adventurer would you like to login or sign up?");
       String login = input.nextLine();
+      while (true) {
+         if (login.equals("login")) {
+            System.out.println("Username:");
+            String username = input.nextLine();
 
-      if (login.equals("login")) {
-         System.out.println("Username:");
-         String username = input.nextLine();
+            System.out.println("Password:");
+            String password = input.nextLine();
 
-         System.out.println("Password:");
-         String password = input.nextLine();
+            try {
+               Connection connection = DriverManager.getConnection("jdbc:postgresql:project_postgresql", "jasonwoodruff", "8608");
+               PreparedStatement statement = connection.prepareStatement("select * from player where username=? and password=?");
 
-         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql:project_postgresql", "jasonwoodruff", "8608");
-            PreparedStatement statement = connection.prepareStatement("select * from player where username=? and password=?");
+               statement.setString(1, username);
+               statement.setString(2, password);
 
-            statement.setString(1, username);
-            statement.setString(2, password);
-
-            ResultSet resultSet = statement.executeQuery();
-            connection.close();
-            statement.close();
-
-
-            if (resultSet.next()) {
-               System.out.println("Welcome back, " + username + "! You are logged in.");
-            }
-            else {
-               System.out.println("User does not exist.");
-            }
+               ResultSet resultSet = statement.executeQuery();
+               connection.close();
+               statement.close();
 
 
-         }
-         catch (SQLException e) {
-            e.printStackTrace();
-
-            System.exit(1);
-         }
-
-
-      }else if (login.equals("signup")) {
-         System.out.println("Username:");
-         String username = input.nextLine();
-
-         System.out.println("Password:");
-         String password = input.nextLine();
-
-         try{
-            Connection connection = DriverManager.getConnection("jdbc:postgresql:project_postgresql", "jasonwoodruff", "8608");
-            PreparedStatement stmt = connection.prepareStatement("SELECT COUNT(*) FROM player WHERE username=?");
-
-            stmt.setString(1, username);
-
-            ResultSet resultSet = stmt.executeQuery();
-
-            if (resultSet.next()) {
-               if (resultSet.getInt(1) > 0) {
-
-                  System.out.println("User already exists.");
+               if (resultSet.next()) {
+                  System.out.println("Welcome back, " + username + "! You are logged in.");
+               } else {
+                  System.out.println("User does not exist.");
                }
-               else {
 
-                  PreparedStatement statement = connection.prepareStatement("INSERT INTO player(username, password ) VALUES (?,?)");
 
-                  statement.setString(1, username);
-                  statement.setString(2, password);
+            } catch (SQLException e) {
+               e.printStackTrace();
 
-                  int es = statement.executeUpdate();
+               System.exit(1);
+            }
 
-                  if (es == 0) {
-                     System.out.println("Username already exists.");
+
+         } else if (login.equals("signup")) {
+            System.out.println("Username:");
+            String username = input.nextLine();
+
+            System.out.println("Password:");
+            String password = input.nextLine();
+
+            try {
+               Connection connection = DriverManager.getConnection("jdbc:postgresql:project_postgresql", "jasonwoodruff", "8608");
+               PreparedStatement stmt = connection.prepareStatement("SELECT COUNT(*) FROM player WHERE username=?");
+
+               stmt.setString(1, username);
+
+               ResultSet resultSet = stmt.executeQuery();
+
+               if (resultSet.next()) {
+                  if (resultSet.getInt(1) > 0) {
+
+                     System.out.println("User already exists.");
+                  } else {
+
+                     PreparedStatement statement = connection.prepareStatement("INSERT INTO player(username, password ) VALUES (?,?)");
+
+                     statement.setString(1, username);
+                     statement.setString(2, password);
+
+                     int es = statement.executeUpdate();
+
+                     if (es == 0) {
+                        System.out.println("Username already exists.");
+
+                     } else {
+
+                        System.out.println("Successfully logged in.");
+                     }
+
+                     connection.close();
+                     statement.close();
 
                   }
-                  else {
-
-                     System.out.println("Successfully logged in.");
-                  }
-
-                  connection.close();
-                  statement.close();
-
                }
+
+            } catch (SQLException e) {
+
+               e.printStackTrace();
+               System.exit(2);
             }
-
-         }catch (SQLException e) {
-
-            e.printStackTrace();
-            System.exit(2);
          }
+
       }
-
    }
 }
---
+++ src/start_of_story.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.sql.*;
+
+public class start_of_story {
+    public start_of_story() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mission Log: Solo Assignment – Underwater Facility\n" +
+                "\n" +
+                "The briefing was clear: this was to be a solo mission.\n" +
+                "\n" +
+                "There was no room for a team this time, no backup, and no one to rely on. The cramped conditions of the submersible make it a one-person job—just you, the deep, and the facility beneath the waves.\n" +
+                "\n" +
+                "You were told little about the specifics of the mission. A sense of isolation weighs heavily on you as you prepare for what lies ahead. As the countdown to departure ticks down, you can’t help but wonder if this secrecy is simply part of the plan or if there’s something more ominous at play.\n" +
+                "\n" +
+                "The underwater facility—what’s left of it—remains a mystery. The depth alone would make it nearly impossible for most to access. But you? You were chosen for this mission. It’s your skills they trust. Yet, there’s an unsettling feeling in the back of your mind. You’ll be alone in the depths—out of contact, out of reach, cut off from the world above.\n" +
+                "\n" +
+                "In the final moments before you descend, the hatch closes with a heavy, final thud. There’s no turning back now. Your journey into the abyss is about to begin.");
+    }
+}
