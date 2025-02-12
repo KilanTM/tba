c415b82045ca5ce118ee71df91af430a5013a504
Jason Woodruff <jwoodruff25@basecampcodingacademy.org>
2025-02-12 12:45:31 -0600

    signup /login

---
+++ .idea/sql/schema.sql
@@ -0,0 +1,26 @@
+-- Locations table
+CREATE TABLE locations
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255),
+    description VARCHAR(1000)
+);
+
+-- Inventory table
+CREATE TABLE inv
+(
+    item_id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(1000)
+);
+
+-- Player table
+CREATE TABLE player
+(
+    id               SERIAL PRIMARY KEY,
+    current_location INT REFERENCES locations(id),
+    username         VARCHAR(20) NOT NULL,
+    password         VARCHAR(1000) NOT NULL,
+    inv INT []-- Using an array of item IDs for the player's inventory
+);
+
